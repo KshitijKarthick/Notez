bbbe6c35e7d38e1db08326a4347cd2dc91cf221f
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-11-08 19:46:09 +0530

    IndexedDB support introduced
    * DexieJs wrapper for indexedDB utilisation.
    * Create, Delete functionality for notes using indexedDB.
    * Preload previously stored indexedDB notes.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,75 +8,67 @@
  * Controller of the notezApp
  */
 angular.module('notezApp')
-    .controller('NotesController', ['$timeout',
-        function($timeout) {
-        	this.showAlert = false;
-        	this.alertMessage = "";
-        	this.title = "";
-        	this.content = "";
-            this.notesList = [{
-                'title': 'Note 1',
-                'content': 'Contents of Note 1',
-                'id': 1,
-                'timestamp': (new Date()).getTime()
-            }, {
-                'title': 'Note 2',
-                'content': 'Contents of Note 1',
-                'id': 2,
-                'timestamp': (new Date()).getTime()
-            }, {
-                'title': 'Note 3',
-                'content': 'Contents of Note 1',
-                'id': 3,
-                'timestamp': (new Date()).getTime()
-            }, {
-                'title': 'Note 4',
-                'content': 'Contents of Note 1',
-                'id': 4,
-                'timestamp': (new Date()).getTime()
-            }];
+    .controller('NotesController', ['$timeout', function($timeout) {
+            this.showAlert = false;
+            this.alertMessage = "";
+            this.title = "";
+            this.content = "";
+            var db = new Dexie("notez");
+            db.version(1).stores({
+                notez : 'id,title,content,timestamp',
+            });
+            db.open().catch(function(error){
+                console.error('IndexedDB support not present',error);
+            });
+            this.notesList = [];
+            db.notez.each(function(note){
+                $timeout.apply(this.notesList.push(note));
+            }.bind(this));
             this.addNote = function() {
                 function hashCode(str) {
                     var hash = 5381;
-    				for (var i = 0; i < str.length; i++) {
-        				var char = str.charCodeAt(i);
-        				hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
-    				}
-    				return hash;
+                    for (var i = 0; i < str.length; i++) {
+                        var char = str.charCodeAt(i);
+                        hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
+                    }
+                    return hash;
                 }
                 var currentTime = (new Date()).getTime();
-                this.notesList.push({
+                var newNote = {
                     'title': this.title,
                     'content': this.content,
                     'timestamp': currentTime,
                     'id': hashCode(this.title + currentTime)
-                });
+                };
+                this.notesList.push(newNote);
+                db.notez.add(newNote);
                 this.alert("Created " + this.title + " successfully");
                 this.title = "";
                 this.content = "";
             };
             this.removeNote = function(id) {
-            	for(var i=0;i<this.notesList.length;i++){
-            		if(this.notesList[i].id === id){
-            			if(i > -1) {
-    						var removedItem = this.notesList.splice(i, 1);
-    						this.alert("Deleted " + removedItem[0].title + " successfully");
-    						break;
-						}
-            		}
-            	}
+                for (var i = 0; i < this.notesList.length; i++) {
+                    if (this.notesList[i].id === id) {
+                        if (i > -1) {
+                            var removedItem = this.notesList.splice(i, 1);
+                            db.notez.delete(removedItem[0].id);
+                            this.alert("Deleted " + removedItem[0].title + " successfully");
+                            break;
+                        }
+                    }
+                }
             };
-            this.alert = function(msg){
-            	this.alertMessage = msg;
-            	this.showAlert = true;
-            	$timeout(function(){
-            		this.showAlert = false;
-            	}.bind(this), 3000);
+            this.alert = function(msg) {
+                this.alertMessage = msg;
+                this.showAlert = true;
+                $timeout(function() {
+                    this.showAlert = false;
+                }.bind(this), 3000);
             };
         }
     ])
-    .controller("HeaderController", function($scope, $location){ 
-        $scope.isActive = function (viewLocation) { 
+    .controller("HeaderController", function($scope, $location) {
+        $scope.isActive = function(viewLocation) {
             return viewLocation === $location.path();
         };
     });
