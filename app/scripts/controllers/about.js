9a9b2bc1e4e133f0df8381cb5af38b4f91b4d2bb
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-11-08 10:28:49 +0530

    Using Yeoman for scaffolding
    * Using yeoman to scaffold a AngularJs application.
    * Refactored code to move to AngularJs.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name notezApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the notezApp
+ */
+angular.module('notezApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name notezApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the notezApp
+ */
+angular.module('notezApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
