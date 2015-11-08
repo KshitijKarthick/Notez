8a7a4e8cbf5495d5c562412db99beec320f334b7
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-11-08 10:39:09 +0530

    Basic note functionality added
    * Create a note.
    * Remove a note.
    * Modal window for input for the new note.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,8 +21,8 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        controller: 'NotesController',
+        controllerAs: 'notes'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,75 @@
 
 /**
  * @ngdoc function
- * @name notezApp.controller:MainCtrl
+ * @name notezApp.controller:NotesController
  * @description
- * # MainCtrl
+ * # NotezController
  * Controller of the notezApp
  */
 angular.module('notezApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('NotesController', ['$timeout',
+        function($timeout) {
+        	this.showAlert = false;
+        	this.alertMessage = "";
+        	this.title = "";
+        	this.content = "";
+            this.notesList = [{
+                'title': 'Note 1',
+                'content': 'Contents of Note 1',
+                'id': 1,
+                'timestamp': (new Date()).getTime()
+            }, {
+                'title': 'Note 2',
+                'content': 'Contents of Note 1',
+                'id': 2,
+                'timestamp': (new Date()).getTime()
+            }, {
+                'title': 'Note 3',
+                'content': 'Contents of Note 1',
+                'id': 3,
+                'timestamp': (new Date()).getTime()
+            }, {
+                'title': 'Note 4',
+                'content': 'Contents of Note 1',
+                'id': 4,
+                'timestamp': (new Date()).getTime()
+            }];
+            this.addNote = function() {
+                function hashCode(str) {
+                    var hash = 5381;
+    				for (var i = 0; i < str.length; i++) {
+        				var char = str.charCodeAt(i);
+        				hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
+    				}
+    				return hash;
+                }
+                var currentTime = (new Date()).getTime();
+                this.notesList.push({
+                    'title': this.title,
+                    'content': this.content,
+                    'timestamp': currentTime,
+                    'id': hashCode(this.title + currentTime)
+                });
+                this.title = "";
+                this.content = "";
+            };
+            this.removeNote = function(id) {
+            	for(var i=0;i<this.notesList.length;i++){
+            		if(this.notesList[i].id === id){
+            			if(i > -1) {
+    						var removedItem = this.notesList.splice(i, 1);
+    						this.alert("Deleted " + removedItem[0].title + " successfully");
+    						break;
+						}
+            		}
+            	}
+            };
+            this.alert = function(msg){
+            	this.alertMessage = msg;
+            	this.showAlert = true;
+            	$timeout(function(){
+            		this.showAlert = false;
+            	}.bind(this), 3000);
+            };
+        }
+    ]);
