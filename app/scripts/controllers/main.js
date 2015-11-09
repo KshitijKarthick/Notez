94f43034a07b712857c150785901d037f4abaadd
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-11-09 23:06:59 +0530

    Edit functionality added
    * Edit/Append functionality added for the notes.
    * Service for handling modal created.
    * Generic modal used for both create and save modal.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,22 @@
  * Controller of the notezApp
  */
 angular.module('notezApp')
-    .controller('NotesController', ['$timeout', function($timeout) {
+    .controller('NotesController', ['$timeout', 'modal',
+        function($timeout, modal) {
             this.showAlert = false;
             this.alertMessage = "";
-            this.title = "";
-            this.content = "";
-            var db = new Dexie("notez");
-            db.version(1).stores({
-                notez : 'id,title,content,timestamp',
+            this.selectedNoteTitle = "";
+            this.selectedNoteContent = "";
+            this.modal = modal;
+            this.db = new Dexie("notez");
+            this.db.version(1).stores({
+                notez: 'id,title,content,timestamp',
             });
-            db.open().catch(function(error){
-                console.error('IndexedDB support not present',error);
+            this.db.open().catch(function(error) {
+                console.error('IndexedDB support not present', error);
             });
             this.notesList = [];
-            db.notez.each(function(note){
+            this.db.notez.each(function(note) {
                 $timeout.apply(this.notesList.push(note));
             }.bind(this));
             this.addNote = function() {
@@ -35,29 +37,53 @@ angular.module('notezApp')
                 }
                 var currentTime = (new Date()).getTime();
                 var newNote = {
-                    'title': this.title,
-                    'content': this.content,
+                    'title': this.selectedNoteTitle,
+                    'content': this.selectedNoteContent,
                     'timestamp': currentTime,
-                    'id': hashCode(this.title + currentTime)
+                    'id': hashCode(this.selectedNoteTitle + currentTime)
                 };
                 this.notesList.push(newNote);
-                db.notez.add(newNote);
-                this.alert("Created " + this.title + " successfully");
-                this.title = "";
-                this.content = "";
+                this.db.notez.add(newNote);
+                this.alert("Created " + this.selectedNoteTitle + " successfully");
+                this.selectedNoteTitle = "";
+                this.selectedNoteContent = "";
             };
             this.removeNote = function(id) {
                 for (var i = 0; i < this.notesList.length; i++) {
                     if (this.notesList[i].id === id) {
                         if (i > -1) {
                             var removedItem = this.notesList.splice(i, 1);
-                            db.notez.delete(removedItem[0].id);
+                            this.db.notez.delete(removedItem[0].id);
                             this.alert("Deleted " + removedItem[0].title + " successfully");
                             break;
                         }
                     }
                 }
             };
+            this.editNote = function(id) {
+                for (var i = 0; i < this.notesList.length; i++) {
+                    if (this.notesList[i].id === id) {
+                        if (i > -1) {
+                            this.selectedNoteTitle = this.notesList[i].title;
+                            this.selectedNoteContent = this.notesList[i].content;
+                            this.modal.setModalData('Edit a Note', 'Save a Note', function() {
+                                this.notesList[i].title = this.selectedNoteTitle;
+                                this.notesList[i].content = this.selectedNoteContent;
+                                this.notesList[i].timestamp = (new Date()).getTime();
+                                this.db.notez.update(this.notesList[i].id, this.notesList[i]);
+                                this.selectedNoteTitle = "";
+                                this.selectedNoteContent = "";
+                            }.bind(this));
+                            break;
+                        }
+                    }
+                }
+            };
+            this.invokeCreationofNote = function() {
+                this.modal.setModalData('Create a Note', 'Create Note', function() {
+                    this.addNote();
+                }.bind(this));
+            }
             this.alert = function(msg) {
                 this.alertMessage = msg;
                 this.showAlert = true;
@@ -67,8 +93,49 @@ angular.module('notezApp')
             };
         }
     ])
+    .factory('modal', function() {
+        var modalTitle = "Title";
+        var actionButtonTitle = "Do Task";
+        var action = function() {
+            console.log("Hello User");
+        }
+        var setModalData = function(title, buttonTitle, newAction) {
+            modalTitle = title;
+            actionButtonTitle = buttonTitle;
+            action = newAction;
+        }
+        var getModalTitle = function() {
+            return modalTitle;
+        }
+        var getActionButtonTitle = function() {
+            return actionButtonTitle;
+        }
+        var getAction = function() {
+            return action;
+        }
+        return {
+            'getModalTitle': getModalTitle,
+            'getActionButtonTitle': getActionButtonTitle,
+            'setModalData': setModalData,
+            'action': action,
+            'getAction': getAction
+        };
+    })
     .controller("HeaderController", function($scope, $location) {
         $scope.isActive = function(viewLocation) {
             return viewLocation === $location.path();
+        };
+    })
+    .filter('orderObjectBy', function() {
+        return function(items, field, reverse) {
+            var filtered = [];
+            angular.forEach(items, function(item) {
+                filtered.push(item);
+            });
+            filtered.sort(function(a, b) {
+                return (a[field] > b[field] ? 1 : -1);
+            });
+            if (reverse) filtered.reverse();
+            return filtered;
         };
     });
